f3c720cd6cd5dc54e6c5c4a46de090a8ba86bab5
jQN <owner@Owners-MacBook-Pro.local>
2015-07-03 12:51:19 -0600

    Made d3 trianble responsive

---
+++ siambiosys-custom-scripts.js
@@ -0,0 +1,8 @@
+var chart = $("#d3-svg"),
+    aspect = chart.width() / chart.height(),
+    container = chart.parent();
+$(window).on("resize", function() {
+    var targetWidth = container.width();
+    chart.attr("width", targetWidth);
+    chart.attr("height", Math.round(targetWidth / aspect));
+}).trigger("resize");
